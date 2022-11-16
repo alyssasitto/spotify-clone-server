927d59b6a6af142e43eed5c2e3af050bf516d09f
Alyssa Sitto <alyssasitto1@gmail.com>
2022-11-15 23:53:05 -0800

    Add routes for getting playlists

--- routes/spotify.routes.js
+++ routes/spotify.routes.js
@@ -1,15 +1,32 @@
 const router = require("express").Router();
 const axios = require("axios");
 const SpotifyWebApi = require("spotify-web-api-node");
-const { response } = require("../app");
 
 // route for getting top items
-router.get("/top-items/:token", (req, res) => {
-	const token = req.params.token;
-	const spotifyApi = new SpotifyWebApi({ accessToken: token });
+router.get("/top-items", (req, res) => {
+	const token = req.headers.token;
 
 	axios
-		.get("https://api.spotify.com/v1/me/top/tracks", {
+		.get("https://api.spotify.com/v1/me/top/tracks?limit=50", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		.then((response) => {
+			console.log("TOP ITEMS", response);
+			res.status(200).json(response.data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// route for getting most recent songs
+router.get("/recents", (req, res) => {
+	const token = req.headers.token;
+
+	axios
+		.get("https://api.spotify.com/v1/me/player/recently-played?limit=50", {
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
@@ -20,19 +37,106 @@ router.get("/top-items/:token", (req, res) => {
 		.catch((err) => {
 			console.log(err);
 		});
-
-	console.log("THIS IS THE TOKEN", token);
 });
 
+// route for getting albums by their id
 router.get("/albums", (req, res) => {
 	const spotifyApi = new SpotifyWebApi({ accessToken: req.headers.token });
-
-	console.log(req.headers.albums);
 
 	spotifyApi
 		.getAlbums([req.headers.albums])
 		.then((response) => {
-			console.log(response);
+			console.log("ALBUMS", response);
+			res.status(200).json(response.body);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// route for getting featured playlists
+router.get("/featured-playlists", (req, res) => {
+	const token = req.headers.token;
+
+	axios
+		.get("https://api.spotify.com/v1/browse/featured-playlists?limit=50", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		.then((response) => {
+			res.status(200).json(response.data);
+		})
+
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// route for getting new releases
+router.get("/new-releases", (req, res) => {
+	const token = req.headers.token;
+
+	axios
+		.get("https://api.spotify.com/v1/browse/new-releases?limit=50", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		.then((response) => {
+			res.status(200).json(response.data);
+			console.log("NEW RELEASES", response);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// route for getting categories
+router.get("/categories", (req, res) => {
+	const spotifyApi = new SpotifyWebApi({ accessToken: req.headers.token });
+
+	console.log("THIS IS THE TOKENNN ======>", req.headers.token);
+
+	spotifyApi
+		.getCategories({
+			limit: 50,
+			country: "US",
+		})
+		.then((response) => {
+			res.status(200).json(response.body);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// route for getting recent playlists
+router.get("/playlists", (req, res) => {
+	console.log("TOKENNNN ====>", req.headers.token);
+
+	const spotifyApi = new SpotifyWebApi({ accessToken: req.headers.token });
+
+	spotifyApi
+		.getUserPlaylists({
+			limit: 50,
+		})
+		.then((response) => {
+			res.status(200).json(response.body);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// route for getting single playlist
+router.get("/playlist/:id", (req, res) => {
+	const id = req.params.id;
+	const spotifyApi = new SpotifyWebApi({ accessToken: req.headers.token });
+
+	spotifyApi
+		.getPlaylist(id)
+		.then((response) => {
 			res.status(200).json(response.body);
 		})
 		.catch((err) => {
@@ -41,3 +145,15 @@ router.get("/albums", (req, res) => {
 });
 
 module.exports = router;
+
+// .then((response) => {
+// 	return axios.get("https://api.spotify.com/v1/browse/new-releases", {
+// 		headers: {
+// 			Authorization: `Bearer ${token}`,
+// 		},
+// 	});
+// })
+// .then((response) => {
+// 	console.log(response);
+// 	res.status(200).json(response.data);
+// })
